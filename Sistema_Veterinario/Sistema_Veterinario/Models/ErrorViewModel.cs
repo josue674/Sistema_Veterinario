bd7f5d0a38bec23079ca2ae7634614837caccd1c
josue674 <71514774+josue674@users.noreply.github.com>
2024-03-02 16:32:23 -0600

    Creación de proyecto
    - Creación de proyecto MVP
    - Instalación de Dependencias

---
+++ Sistema_Veterinario/Sistema_Veterinario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_Veterinario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

0f6d94bf6b46899cf732f63e5e24fa4983f4be07
Melanie Picado <melaniepicado30@gmail.com>
2024-03-28 19:52:35 -0600

    .

--- Sistema_Veterinario/Sistema_Veterinario.DAL/obj/Debug/net8.0/Sistema_Veterinario.DAL.AssemblyInfo.cs
+++ Sistema_Veterinario/Sistema_Veterinario.DAL/obj/Debug/net8.0/Sistema_Veterinario.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_Veterinario.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6890c4129bded4fd100d7e1bc2a649b4b52e56de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba816bfcb2505a8293206dbc564916a2285d00f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_Veterinario.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema_Veterinario.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

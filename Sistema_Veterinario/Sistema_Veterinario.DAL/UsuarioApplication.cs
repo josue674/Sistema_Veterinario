4e535a0d2d1ce665c77b342a545c5b05090dfed7
Gretchen <gretks@hotmail.com>
2024-03-31 13:47:36 -0600

    Autentication

---
+++ Sistema_Veterinario/Sistema_Veterinario.DAL/UsuarioApplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Veterinario.DAL
+{
+    internal class UsuarioApplication : IdentityUser
+    {
+
+    }
+}
